926f095cc8fbd9a4a08a126ece17b7404c55775b
島田則幸 <net_7jp@yahoo.co.jp>
2020-09-05 03:15:56 +0900

    PinBallプロジェクト
    課題：得点を表示しよう

---
+++ Assets/ScoreTextController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreTextController : MonoBehaviour
+{
+    public Text ScoreText;
+    private int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        SetScore();
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        string myTag = collision.gameObject.tag;
+        if (myTag == "SmallStarTag")
+        {
+            score += 10;
+        }else if (myTag == "LargeStarTag")
+        {
+            score += 20;
+        }else if (myTag == "SmallCloudTag")
+        {
+            score += 50;
+        }else if (myTag == "LargeCloudTag")
+        {
+            score += 100;
+        }
+        SetScore();
+    }
+    void SetScore()
+    {
+        ScoreText.text = string.Format("Score:{0}", score);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
